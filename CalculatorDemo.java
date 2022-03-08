1a36822904b46bee7d05cf7239cf1732ec886c2e
baristekinn0 <101211661+baristekinn0@users.noreply.github.com>
2022-03-08 21:52:29 +0300

    Add files via upload

---
+++ CalculatorDemo.java
@@ -0,0 +1,48 @@
+package deneme;
+
+import java.util.Scanner;
+
+public class CalculatorDemo {
+
+	public static void main(String[] args) {
+		Scanner okuyucu = new Scanner(System.in);
+		System.out.println("Hesap makineme hoþgeldiniz");
+		System.out.println("Ýþlem yapmak istediðiniz iki sayýyý giriniz");
+		int num1 = okuyucu.nextInt();
+		int num2 = okuyucu.nextInt();
+		System.out.println("Hangi iþlemi yapmak istersiniz?");
+		char islem = okuyucu.next().charAt(0);
+		double sonuc;
+		switch(islem) {
+		
+		case '+':
+			
+			sonuc = num1 + num2;
+			System.out.println(num1 + "+" + num2 + "=" + sonuc);
+			break;
+		
+		case '-':
+			
+			sonuc = num1 - num2;
+			System.out.println(num1 + "-" + num2 + "=" + sonuc);
+			break;
+			
+		case '*':
+				
+				sonuc = num1 * num2;
+				System.out.println(num1 + "*" + num2 + "=" + sonuc);
+				break;
+		case '/':
+			
+			sonuc = num1 / num2;
+			System.out.println(num1 + "/" + num2 + "=" + sonuc);
+			break;
+			
+			default: System.out.println("Geçerli iþlemlerden seçiniz: + , - , * , /");
+			
+		
+		}
+		okuyucu.close();
+	}
+
+}
